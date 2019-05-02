10252e22e01ed55059c454bd9430a38dee6d0ae7
sadeghpro <sadeghmohamadnia@yahoo.com>
2019-05-02 18:16:14 +0430

    Add setOnLastItemVisible method for vlist and loading for that

--- vlist/src/main/java/ir/sadeghpro/husky/vlist/VList.java
+++ vlist/src/main/java/ir/sadeghpro/husky/vlist/VList.java
@@ -1,17 +1,24 @@
 package ir.sadeghpro.husky.vlist;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.widget.AdapterView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.tbuonomo.materialsquareloading.MaterialSquareLoading;
 
 public class VList extends ListView {
 
     private int cellCount;
+    private boolean loading;
+    private boolean finish;
 
     private OnItemClickListener onItemClickListener;
 
@@ -36,7 +43,7 @@ public class VList extends ListView {
     public void setAdapter(ListAdapter adapter) {
         if (adapter instanceof VListAdapterHelper) {
             super.setAdapter(adapter);
-            if (cellCount!=0){
+            if (cellCount != 0) {
                 ((VListAdapterHelper) getAdapter()).setCellCount(cellCount);
             }
         } else {
@@ -60,10 +67,79 @@ public class VList extends ListView {
     }
 
     public void setCellCount(int cellCount) {
-        if (getAdapter() != null){
+        if (getAdapter() != null) {
             ((VListAdapterHelper) getAdapter()).setCellCount(cellCount);
         } else {
             this.cellCount = cellCount;
         }
+    }
+
+    public void setOnLastItemVisible(final OnLastItemVisible onLastItemVisible) {
+        setOnLastItemVisible(false, null, onLastItemVisible);
+    }
+
+    public void setOnLastItemVisible(final boolean enableLoading, final LoadingType type, final OnLastItemVisible onLastItemVisible) {
+        setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem + visibleItemCount == totalItemCount && totalItemCount != 0 && !loading && !finish) {
+                    loading = true;
+                    View loadingView = null;
+                    if (enableLoading) {
+                        switch (type) {
+                            case CIRCLE:
+                                loadingView = LayoutInflater.from(getContext()).inflate(R.layout.circle_loading, VList.this, false);
+                                break;
+                            case SQUARE:
+                            default:
+                                loadingView = LayoutInflater.from(getContext()).inflate(R.layout.square_loading, VList.this, false);
+                                MaterialSquareLoading materialSquareLoading = loadingView.findViewById(R.id.material_square_loading_view);
+                                materialSquareLoading.show();
+                                break;
+                        }
+                        addFooterView(loadingView);
+                    }
+                    final View finalLoadingView = loadingView;
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            onLastItemVisible.lastItemVisible();
+                            loading = false;
+                            ((Activity) getContext()).runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (enableLoading) {
+                                        removeFooterView(finalLoadingView);
+                                    }
+                                    ((VListAdapterHelper) getAdapter()).notifyDataSetChanged();
+                                }
+                            });
+                        }
+                    }).start();
+                }
+            }
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+        });
+    }
+
+    public interface OnLastItemVisible {
+        void lastItemVisible();
+    }
+
+    public boolean isFinish() {
+        return finish;
+    }
+
+    public void setFinish(boolean finish) {
+        this.finish = finish;
+    }
+
+    public enum LoadingType {
+        CIRCLE,
+        SQUARE
     }
 }
