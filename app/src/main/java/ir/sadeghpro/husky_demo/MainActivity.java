cb4d6f1f9d18e8a6db9f3d025e234536cfb242f4
sadeghpro <sadeghmohamadnia@yahoo.com>
2019-05-03 23:01:00 +0430

    Fix slider bug when restart auto play

--- app/src/main/java/ir/sadeghpro/husky_demo/MainActivity.java
+++ app/src/main/java/ir/sadeghpro/husky_demo/MainActivity.java
@@ -24,12 +24,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Slider slider = findViewById(R.id.slider);
+        final Slider slider = findViewById(R.id.slider);
         List<SliderModel> modelList = new ArrayList<>();
         modelList.add(new SimpleModel("https://www.ctvnews.ca/polopoly_fs/1.4037876!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg"));
         modelList.add(new SimpleModel("https://d1q6f0aelx0por.cloudfront.net/product-logos/81630ec2-d253-4eb2-b36c-eb54072cb8d6-golang.png"));
         modelList.add(new SimpleModel("http://images.math.cnrs.fr/IMG/png/section8-image.png"));
         slider.setModel(modelList);
+        slider.setAutoPlay(true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    slider.setAutoPlay(false);
+                    Thread.sleep(5000);
+                    slider.setAutoPlay(true);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
 
         HList list = findViewById(R.id.list);
         ArrayList<Model> models = new ArrayList<>();
--- slider/src/main/java/ir/sadeghpro/husky/slider/Slider.java
+++ slider/src/main/java/ir/sadeghpro/husky/slider/Slider.java
@@ -119,6 +119,7 @@ public class Slider extends ConstraintLayout {
                                 }
                             });
                         }
+                        thread = null;
                     }
                 });
                 thread.start();
